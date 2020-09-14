f6f7d84576ede5ac0a0fd5921c7b80ae75f41fef
zubat187 <krotilia3@gmail.com>
2020-09-14 10:03:09 +0000

    task 4 5

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 using namespace std;
 
+void task5(){
+  int a;
+  int b;
+  int c;
+  cout << "VVedite chisla" << endl;
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  if (1<=a && a<=3)
+  cout <<" a podhodit" <<endl;
+  else if (1<=b && b<=3)
+  cout <<"b podhodit" <<endl;
+  else if (1<=c && c<=3)
+  cout <<"c podhodit" <<endl;
+
+}
 int main(){
 int A;
 cout <<"Vvedite chislo A" <<endl;
@@ -10,5 +26,6 @@ if (A%2 == 0 )
 cout <<"Chislo chetnoe";
 else if (A%2 >= 1 )
 cout <<"Chislo nechetnoe";
+task5();
 return 0;
 }
